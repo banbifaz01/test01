ac0bf4fe94a88a17f8d96a3e58a11d74da43add3
banbifaz01 <banbifaz01@outlook.com>
2018-09-07 13:52:17 -0300

    update config

--- config.php
+++ config.php
@@ -5,10 +5,10 @@
  *
  */
 
-$host       = "localhost";
-$username   = "root";
-$password   = "root";
-$dbname     = "test";
+$host       = "dbserverbanbifaz01-mysqldbserver.mysql.database.azure.com";
+$username   = "mysqldbuser@dbserverbanbifaz01-mysqldbserver";
+$password   = "83nT>-pM%{uqUPK&";
+$dbname     = "dbbanbifaz01";
 $dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
